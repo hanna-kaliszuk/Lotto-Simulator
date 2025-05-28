fc1323f01bce630dce8c11264155a3f4f1209e70
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-05-28 17:56:46 +0200

    zakład wstępnie zaimplementowany

--- src/główny/Main.java
+++ src/główny/Main.java
@@ -1,10 +1,11 @@
 package główny;
 
 import centrala.losowanie.Losowanie;
+import kupony.Zakład;
 
 public class Main {
     private static void testLosowania() {
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 2137; i++) {
             Losowanie l = new Losowanie();
             System.out.println(l);
         }
@@ -12,8 +13,16 @@ public class Main {
         Losowanie l = new Losowanie();
         System.out.println(l);
     }
+
+    private static void testZakładów() {
+        Zakład z = new Zakład(new int[] {1, 2, 3, 4, 5, 6});
+        int[] wylosowane = new int[] {-2, -4, -6, -8, 10, 12};
+        System.out.println(z.ileTrafień(wylosowane));
+        System.out.println(z);
+    }
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!\n");
-        testLosowania();
+        //testLosowania();
+        testZakładów();
     }
 }
--- src/kupony/Zakład.java
+++ src/kupony/Zakład.java
@@ -1,4 +1,39 @@
 package kupony;
 
+import java.util.Arrays;
+
 public class Zakład {
+    private final int[] liczby;
+    boolean ważny;
+
+    public Zakład(int[] liczby) {
+        if (liczby.length == 6) {
+            this.liczby = Arrays.copyOf(liczby, liczby.length);
+            Arrays.sort(this.liczby);
+            this.ważny = true;
+        } else {
+            this.ważny = false;
+            this.liczby = null;
+        }
+    }
+
+    public int ileTrafień(int[] wylosowane) {
+        int trafienia = 0;
+        for (int liczba : liczby) {
+            if (Arrays.binarySearch(wylosowane, liczba) >= 0) {
+                trafienia++;
+            }
+        }
+
+        return trafienia;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < liczby.length; i++) {
+            sb.append(String.format("%2d", liczby[i]));
+            if (i < liczby.length - 1) sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
