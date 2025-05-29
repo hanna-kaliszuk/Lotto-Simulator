b8858914879e8abf9de31c4ff53013e1f6ae8127
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-05-29 18:45:11 +0200

    centrala udostępnia nr następnego losowania

--- src/centrala/Centrala.java
+++ src/centrala/Centrala.java
@@ -1,5 +1,32 @@
 package centrala;
 
+import centrala.losowanie.Losowanie;
+import finanse.Kwota;
+import kolektura.Kolektura;
+import java.util.List;
+
+
 public class Centrala {
+    private Kwota środkiFinansowe;
+    private List<Kolektura> listaKolektur;
+    private int nrOstatniegoLosowania = 0;
+
+    public Centrala(Kwota kwota, List<Kolektura> kolektury) {
+        this.środkiFinansowe = kwota;
+        this.listaKolektur = kolektury;
+    }
+
+    public void przeprowadźLosowanie() {
+        Losowanie losowanie = new Losowanie();
+        this.nrOstatniegoLosowania = losowanie.getNrLosowania();
+    }
+
+    public Kwota obliczPulęNagród() {
+
+    }
+
+    public int getNrNastępnegoLosowania() {
+        return this.nrOstatniegoLosowania + 1;
+    }
 
 }
