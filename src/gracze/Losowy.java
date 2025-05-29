bffae71b298495f53a3511011c2af5006f8c426d
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-05-29 21:33:48 +0200

    zarys losowego

--- src/gracze/Losowy.java
+++ src/gracze/Losowy.java
@@ -1,4 +1,27 @@
 package gracze;
 
+import finanse.Kwota;
+import kupony.Zakład;
+
+import java.util.Random;
+
 public class Losowy extends Gracz {
+    public Losowy(String imię, String nazwisko, int pesel){
+        super(imię, nazwisko, pesel, generujLosoweŚrodki());
+    }
+
+    private static Kwota generujLosoweŚrodki() {
+        int złote = random.nextInt(100000);
+        int grosze = random.nextInt(100);
+
+        return new Kwota(złote, grosze);
+    }
+
+
+    @Override
+    public void kupKupon() {
+        int liczbaKuponów = random.nextInt(100) + 1;
+
+
+    }
 }
