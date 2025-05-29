a5253ec6bdd46a41bde52d7e8a016cb19a550457
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-05-29 18:44:53 +0200

    minimalista umie kupić kupon

--- src/gracze/Gracz.java
+++ src/gracze/Gracz.java
@@ -1,9 +1,99 @@
 package gracze;
 
-public class Gracz {
+import centrala.Centrala;
+import finanse.Kwota;
+import kolektura.Kolektura;
+import kupony.Kupon;
+import kupony.Zakład;
+
+import java.util.*;
+
+public abstract class Gracz {
     protected String imię;
     protected String nazwisko;
     protected int pesel;
-//    protected (typ) środki;
-//    protected (typ) zakupioneKupony;
+    protected Kwota środki;
+    protected List<Kupon> posiadaneKupony;
+    protected final Random r = new Random();
+    protected Centrala centrala;
+
+    public Gracz(String imię, String nazwisko, int pesel, Kwota środki) {
+        this.imię = imię;
+        this.nazwisko = nazwisko;
+        this.pesel = pesel;
+        this.środki = środki;
+        this.posiadaneKupony = new LinkedList<>(); // początkowo gracz nie ma żadnych kuponów
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        // informacje podstawowe o graczu
+        sb.append(imię).append(" ").append(nazwisko).append(" (PESEL: ").append(pesel).append(")\n");
+
+        // informacje o środkach
+        sb.append("Środki: ").append(środki.toString()).append("\n");
+
+        // informacje o kuponach
+        if (posiadaneKupony.isEmpty()) {
+            sb.append("Nie posiada żadnych kuponów.");
+        } else {
+            sb.append("Posiadane kupony:\n");
+            for (Kupon kupon : posiadaneKupony) {
+                sb.append(" - ").append(kupon.getIdentyfikator());
+
+                // informacja o wykorzystaniu kuponu
+                if (kupon.czyWykorzystany()) {
+                    sb.append(" (zrealizowany)");
+                }
+                sb.append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public abstract void kupKupon();
+
+    public abstract Zakład obstawZakład();
+
+    protected void dodajKupon(Kupon kupon) {
+        posiadaneKupony.add(kupon);
+    }
+
+    protected boolean możeKupićKupon(Kwota cenaKuponu) {
+        return maWystarczająceŚrodki(cenaKuponu);
+    }
+
+    private boolean maWystarczająceŚrodki(Kwota cena) {
+        return środki.porównaj(cena) >= 0;
+    }
+
+    protected void odejmijŚrodki(Kwota kwota) {
+        this.środki.odejmij(kwota);
+    }
+
+    protected int[] generujLosoweLiczby(int liczba) {
+        int[] liczby = new int[liczba];
+        Set<Integer> wylosowane = new HashSet<>();
+
+        int i = 0;
+        while (i < liczba) {
+            int losowa = r.nextInt(49) + 1; // liczby od 1 do 49
+
+            if (wylosowane.add(losowa)) {
+                liczby[i] = losowa;
+                i++;
+            }
+        }
+
+        // posortuj liczby dla lepszej czytelności
+        Arrays.sort(liczby);
+        return liczby;
+
+    }
+
+
 }
--- src/gracze/Minimalista.java
+++ src/gracze/Minimalista.java
@@ -1,4 +1,40 @@
 package gracze;
 
+import finanse.Kwota;
+import kolektura.Kolektura;
+import kupony.Kupon;
+import kupony.Zakład;
+
 public class Minimalista extends Gracz {
+    private Kolektura ulubionaKolektura;
+
+    public Minimalista(String imię, String nazwisko, int pesel, Kwota środki, Kolektura ulubiona) {
+        super(imię, nazwisko, pesel, środki);
+        this.ulubionaKolektura = ulubiona;
+    }
+
+    @Override
+    public void kupKupon() {
+        // generuj losowy zakład
+        Zakład zakład = this.obstawZakład();
+
+        // stwórz kupon z 1 zakładem na najbliższe losowanie
+        Kupon kupon = new Kupon(ulubionaKolektura, zakład, centrala.getNrNastępnegoLosowania(), 1);
+
+        // sprawdź, czy gracz ma wystarczające środki na zakup kuponu
+        if (!możeKupićKupon(kupon.getCena())) return;
+
+        // jeżeli tak, to wydajemy mu kupon
+        else {
+            ulubionaKolektura.sprzedajKupon(kupon);
+            dodajKupon(kupon);
+            odejmijŚrodki(kupon.getCena());
+        }
+
+    }
+
+    @Override
+    public Zakład obstawZakład() {
+        return new Zakład(generujLosoweLiczby(6));
+    }
 }
