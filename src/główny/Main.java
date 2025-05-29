21328749c48e070810e352fb8096289038c0247e
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-05-29 21:32:33 +0200

    modyfikacja w testach, nieznacząca

--- src/główny/Main.java
+++ src/główny/Main.java
@@ -1,6 +1,7 @@
 package główny;
 
 import centrala.losowanie.Losowanie;
+import kolektura.Kolektura;
 import kupony.Kupon;
 import kupony.Zakład;
 
@@ -19,22 +20,13 @@ public class Main {
     }
 
     private static void testZakładów() {
-        Zakład z = new Zakład(new int[] {1, 2, 3, 4, 5, 6});
+        Zakład z = new Zakład(new int[] {1, 2, 3, 34, 5, 6});
         int[] wylosowane = new int[] {-2, -4, -6, -8, 10, 12};
-        System.out.println(z.ileTrafień(wylosowane));
         System.out.println(z);
     }
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!\n");
-        //testLosowania();
-        //testZakładów();
-        Zakład z = new Zakład(new int[] {11, 12, 19, 23, 33, 43});
-        Zakład z2 = new Zakład(new int[] {4, 15, 24, 33, 35, 44 });
-        List<Zakład> zakłady = new ArrayList<>();
-        zakłady.add(z);
-        zakłady.add(z2);
-
-        Kupon k = new Kupon(1, zakłady, 8, 4);
-        System.out.println(k);
+        testLosowania();
+        testZakładów();
     }
 }
