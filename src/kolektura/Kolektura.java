39a1fe893876d6b8992bc3b8d984843dd5292eb8
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-06-03 19:15:10 +0200

    rozszerzenie konstruktora klasy Kupon o parametr najbliższeLosowanie, aktualizacja wywołań w Kolektura

--- src/kolektura/Kolektura.java
+++ src/kolektura/Kolektura.java
@@ -35,7 +35,7 @@ public class Kolektura {
             throw new IllegalArgumentException("Gracz nie ma wystarczających środków na zakup kuponu.");
         }
 
-        Kupon kupon = new Kupon(this, blankiet.getZakłady(), blankiet.getIleLosowań());
+        Kupon kupon = new Kupon(this, blankiet.getZakłady(), blankiet.getIleLosowań(), najbliższeLosowanie);
         sprzedaneKupony.add(kupon);
 
         this.przekażŚrodki(cena);
@@ -70,7 +70,7 @@ public class Kolektura {
 
         int najbliższeLosowanie = centrala.getNrNastępnegoLosowania();
 
-        Kupon kupon = new Kupon(this, zakłady, liczbaLosowań);
+        Kupon kupon = new Kupon(this, zakłady, liczbaLosowań, najbliższeLosowanie);
         sprzedaneKupony.add(kupon);
 
         this.przekażŚrodki(cena);
