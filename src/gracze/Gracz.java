d89be5b57707b0c7603bbb30126ecc85beb8a011
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-06-03 19:14:50 +0200

    rozszerzenie klasy Gracz o pola imię, nazwisko, pesel, środki i kupony; dodanie metod toString, jestWypłacalny, zapłać i odbierzKupon

--- src/gracze/Gracz.java
+++ src/gracze/Gracz.java
@@ -1,5 +1,61 @@
 package gracze;
 
-public class Gracz {
+import finanse.Kwota;
+import kupony.Kupon;
 
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class Gracz {
+    private String imię;
+    private String nazwisko;
+    private int pesel;
+    private Kwota środki;
+    private List<Kupon> zakupioneKupony;
+
+    public Gracz(String imię, String nazwisko, int pesel, Kwota środki) {
+        this.imię = imię;
+        this.nazwisko = nazwisko;
+        this.pesel = pesel;
+        this.środki = środki;
+        this.zakupioneKupony = new LinkedList<>(); // początkowo gracz nie ma żadnych kuponów
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        // informacje podstawowe o graczu
+        sb.append(imię).append(" ").append(nazwisko).append(" (PESEL: ").append(pesel).append(")\n");
+
+        // informacje o środkach
+        sb.append("Środki: ").append(środki.toString()).append("\n");
+
+        // informacje o kuponach
+        if (zakupioneKupony.isEmpty()) {
+            sb.append("Nie posiada żadnych kuponów.");
+        } else {
+            sb.append("Posiadane kupony:\n");
+
+            for (Kupon kupon : zakupioneKupony) {
+                sb.append(" - ").append(kupon.getIdentyfikator());
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+    public boolean jestWypłacalny(Kwota kwota) {
+        return (this.środki.porównaj(kwota) >= 0);
+    }
+
+
+    public void zapłać(Kwota cena) {
+        this.środki.odejmij(cena);
+    }
+
+    public void odbierzKupon(Kupon kupon) {
+        zakupioneKupony.add(kupon);
+    }
 }
