1ce63f2590187281cc6e0581e5ae20ea75b496a7
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-06-03 18:49:05 +0200

    jestWażny i czyAnulowany w klasie Zakład, dodanie konstruktora

--- src/kupony/Zakład.java
+++ src/kupony/Zakład.java
@@ -1,4 +1,24 @@
 package kupony;
 
+import java.util.Arrays;
+
 public class Zakład {
+    private int[] liczby;
+    private boolean anulowany;
+
+    public Zakład(int[] liczby) {
+        // dodaj sprawdzenie długości tablicy i czy ma 6 różnych liczb z zakresu 1-49
+        this.liczby = Arrays.copyOf(liczby, liczby.length);
+        this.anulowany = false;
+    }
+
+
+    public boolean jestWażny() {
+        return liczby.length == 6;
+    }
+
+
+    public boolean czyAnulowany() {
+        return anulowany;
+    }
 }
