fe8da487d8ce6fab265a69b459ade97295c0c2bc
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-06-03 13:51:52 +0200

    dodanie metody toString w klasie Blankiet

--- src/kupony/Blankiet.java
+++ src/kupony/Blankiet.java
@@ -1,5 +1,33 @@
 package kupony;
 
+import java.util.List;
+
 public class Blankiet {
+    private int ileLosowań;
+    private List<Zakład> zakłady;
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < zakłady.size(); i++) {
+            sb.append(String.format("%d\n", i));
+            sb.append(zakłady.get(i));
+        }
+
+        sb.append("Liczba losowań: ");
+        for (int i = 1; i <= 10; i++) {
+            sb.append(" [ ");
+            if (i - 1 == ileLosowań) {
+                sb.append(String.format("%2d", i));
+            } else {
+                sb.append("--");
+            }
+            sb.append(" ] ");
+        }
+
+        return sb.toString();
+    }
+
 
 }
