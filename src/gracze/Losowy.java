7b968e9b5a44bbc9fb97eaa219eab5bf7e635b27
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-06-04 13:28:30 +0200

    dodanie klasy Losowy z implementacją kupowania losowych kuponów i generowania losowych środków

---
+++ src/gracze/Losowy.java
@@ -0,0 +1,38 @@
+package gracze;
+
+import finanse.Kwota;
+import kolektura.Kolektura;
+
+import java.util.List;
+import java.util.Random;
+
+public class Losowy extends Gracz {
+    private static Random random = new Random();
+    public Losowy(String imię, String nazwisko, int pesel) {
+        super(imię, nazwisko, pesel, wygenerujLosoweŚrodki());
+    }
+
+    private static Kwota wygenerujLosoweŚrodki() {
+        int złote = random.nextInt(1_000_000);
+        int grosze = random.nextInt(100);
+
+        return new Kwota(złote, grosze);
+    }
+
+    @Override
+    public void kupKupon() {
+        Kolektura kolektura = wybierzLosowąKolekturę(ulubioneKolektury);
+        int ileKuponów = random.nextInt(100) + 1; // losujemy liczbę kuponów od 1 do 100
+
+        for (int i = 0; i < ileKuponów; i++) {
+            int ileZakładów = random.nextInt(8) + 1;
+            int ileLosowań = random.nextInt(10) + 1;
+            kolektura.sprzedajKuponChybiłTrafił(ileZakładów, ileLosowań, this);
+        }
+    }
+
+    private Kolektura wybierzLosowąKolekturę(List<Kolektura> ulubione) {
+        int indeks = random.nextInt(ulubione.size());
+        return ulubione.get(indeks);
+    }
+}
